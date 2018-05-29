8d26a165076847c9696a856ec000596498180cfc
github username <github email address>
2018-05-29 13:23:42 -0400

    Final Edit for Tuesday

--- src/components/stockChart.js
+++ src/components/stockChart.js
@@ -92,7 +92,7 @@ class Chart extends React.Component {
             <Line type="monotone" dataKey="ADBE" stroke="red" />
             <Line type="monotone" dataKey="CRM" stroke="green" />
             <Line type="monotone" dataKey="ORCL" stroke="blue" />
-            <Line type="monotone" dataKey="ATVI" stroke="yellow" />
+            <Line type="monotone" dataKey="ATVI" stroke="orange" />
             <Line type="monotone" dataKey="MSFT" stroke="purple" />
             <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
             <Tooltip/>
