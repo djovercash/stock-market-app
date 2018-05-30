42d90b5bf3bd7c0970cdb798d6f8b4315d42e56c
github username <github email address>
2018-05-30 13:44:22 -0400

    Updated for GH pages

--- src/App.js
+++ src/App.js
@@ -5,31 +5,44 @@ import { fetchStock } from './actions/stocks'
 import {connect} from 'react-redux'
 import MainPageContainer from './containers/mainPageContainer'
 
-class App extends Component {
+const App = () => {
+  return (
+    <div className="App">
+      <MainPageContainer />
+    </div>
+  )
+}
+
+export default App
+
+
+
+//GH PAGES WERE CAUSING PROBLEMS WITH ROUTER, BUT BELOW IS HOW TO ADD ROUTER
+// class App extends Component {
 
   // componentDidMount() {
   //   this.props.history.push("/home")
   // }
 
-  render() {
-    return (
-      <div className="App">
-        <Switch>
-          <Route path="/home" render={(routerParams) => {
-            return <MainPageContainer {...routerParams} />
-          }}/>
-        </Switch>
-      </div>
-    )
-  }
-}
-
-
-function mapStateToProps(state) {
-  return {
-    stocks: state.stocks,
-    isLoading: state.isLoading
-  }
-}
-
-export default withRouter(connect(mapStateToProps, {fetchStock})(App));
+//   render() {
+//     return (
+//       <div className="App">
+//         <Switch>
+//           <Route path="/home" render={(routerParams) => {
+//             return <MainPageContainer {...routerParams} />
+//           }}/>
+//         </Switch>
+//       </div>
+//     )
+//   }
+// }
+//
+//
+// function mapStateToProps(state) {
+//   return {
+//     stocks: state.stocks,
+//     isLoading: state.isLoading
+//   }
+// }
+//
+// export default withRouter(connect(mapStateToProps, {fetchStock})(App));
